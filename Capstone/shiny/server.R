71276169caf5d973c32e8166d915c50e6a8a686f
Zach Christensen <zm.christensen@cox.net>
2015-07-25 16:15:58 -0500

    Added test ui view and button press.

--- Capstone/shiny/server.R
+++ Capstone/shiny/server.R
@@ -1,10 +1,17 @@
 library(shiny)
 
 # Define server logic required to draw a histogram
-shinyServer(function(input, output) {
+shinyServer(function(input, output, session) {
   
   createTable <- function(text) {
-    data.frame(strsplit(text, split = " "))
+    if (nchar(text) > 0) {
+      words <- strsplit(text, split = " ")
+      probabilities <- rep(0.24, length(words))
+      
+      df <- data.frame(words, probabilities, row.names = c(), stringsAsFactors = FALSE)
+      colnames(df) <- c("Word", "Probability")
+      df      
+    }
   }
   
   output$table <- renderTable({
@@ -13,6 +20,10 @@ shinyServer(function(input, output) {
 
   output$sentence <- renderText({
     input$text
+  })
+  
+  observeEvent(input$action, {
+    updateTextInput(session, inputId = "text", value = paste(input$text, "new", sep = " "))
   })
     
 })
